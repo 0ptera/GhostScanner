1964b0a115c79b160799f2a75c5f857f18262d4d
Optera <yousei.kyoko+github@gmail.com>
2019-04-15 17:36:35 +0200

    moved scanner into subgroup with other combinators unlocked by circuit network 2

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,12 +1,18 @@
 data:extend({
   {
+    type = "item-subgroup",
+    name = "circuit-network-2",
+    group = "logistics",
+    order = "g-b"
+  },
+  {
     type = "item",
     name = "ghost-scanner",
-    icon = "__GhostScanner__/graphics/icons/ghost-scanner.png",
-    icon_size = 32,    
-    subgroup = "circuit-network",
     place_result="ghost-scanner",
-    order = "c[combinators]-d[ghost-scanner]",
+    icon = "__GhostScanner__/graphics/icons/ghost-scanner.png",
+    icon_size = 32,
+    subgroup = "circuit-network-2",
+    order = "gs-a",
     stack_size= 50,
   }
 })
